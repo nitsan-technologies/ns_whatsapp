4f652ee021f313964f902fb9a1e59426939a4666
NITSAN Technologies <sanjay@nitsan.in>
2021-04-06 18:17:39 +0530

    [TODO] Update author, documentation, live-demo etc.

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,16 +1,16 @@
 <?php
 
 $EM_CONF['ns_whatsapp'] = [
-    'title' => '[NITSAN] Ns Whatsapp',
-    'description' => '[NITSAN]Ns Whatsapp',
+    'title' => '[NITSAN] TYPO3 WhatsApp Extension',
+    'description' => 'TYPO3 WhatsApp Chat allows your visitors to start a WhatsApp conversation, share messages or join a group from your website with a single click. Demo: https://demo.t3terminal.com/t3t-extensions/whatsapp You can download PRO version for more-features & free-support at https://t3terminal.com/typo3-whatsapp-chat-extension',
     'category' => 'plugin',
-    'author' => '',
+    'author' => 'T3: Tanvi Patel, QA: Vandna Makwana',
     'author_email' => '',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.0-10.99.99',
