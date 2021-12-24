316e6a4eff5d78d011d33f6f414c86e55fc8a5a6
NITSAN Technologies <sanjay@nitsan.in>
2021-12-24 18:03:18 +0530

    [TASK] Corrected the Live-Demo

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,9 +2,9 @@
 
 $EM_CONF['ns_whatsapp'] = [
     'title' => '[NITSAN] TYPO3 WhatsApp Extension',
-    'description' => 'TYPO3 WhatsApp Chat allows your visitors to start a WhatsApp conversation, share messages or join a group from your website with a single click. Demo: https://demo.t3terminal.com/t3t-extensions/whatsapp You can download PRO version for more-features & free-support at https://t3terminal.com/typo3-whatsapp-chat-extension',
+    'description' => 'TYPO3 WhatsApp Chat allows your visitors to start a WhatsApp conversation, share messages or join a group from your website with a single click. Live-Demo: https://demo.t3terminal.com/t3t-extensions/social-networking/whats-app You can download PRO version for more-features & free-support at https://t3terminal.com/typo3-whatsapp-chat-extension',
     'category' => 'plugin',
-    'author' => 'NITSAN Technologies Pvt Ltd',
+    'author' => 'Team NITSAN',
     'author_email' => 'sanjay@nitsan.in',
     'author_company' => 'NITSAN Technologies Pvt Ltd',
     'state' => 'stable',
